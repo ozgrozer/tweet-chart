a1056393ef664f2028994fe96b91d782a04e4ce3
Ozgur <ozgr@live.com>
2021-03-02 00:44:36 +0300

    Pass extracted tweet id

--- src/backend/generateImage.js
+++ src/backend/generateImage.js
@@ -1,5 +1,17 @@
 const generateImage = async (req, res) => {
-  const result = { success: false }
+  const { tweetUrl, cryptocurrency } = req.body
+
+  const splitTweetUrl = tweetUrl.split('/')
+  const lastItem = splitTweetUrl[splitTweetUrl.length - 1]
+  const splitLastItem = lastItem.split('?')
+  const tweetId = splitLastItem[0]
+
+  const result = {
+    tweetId,
+    cryptocurrency,
+    success: false
+  }
+
   res.json(result)
 }
 

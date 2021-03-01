1b01b6d3f300f34c1cea958464d01997e02b68e3
Ozgur <ozgr@live.com>
2021-03-02 00:43:40 +0300

    Accept request data as JSON

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 1300
 
+app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
 
 app.listen(port, () => {

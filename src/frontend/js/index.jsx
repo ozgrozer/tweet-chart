8d92a224ad31ef2ff36e0f3b1ba4f65135185129
Ozgur <ozgr@live.com>
2021-03-01 23:51:12 +0300

    Add simple form

--- src/frontend/js/index.jsx
+++ src/frontend/js/index.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { Form, Input, Select } from 'rfv'
 
 import './../css/style.scss'
 
+const validations = {
+  empty: [
+    {
+      rule: 'isLength',
+      args: { min: 1 },
+      invalidFeedback: 'Please provide a value'
+    }
+  ]
+}
+
 const App = () => {
+  const [formIsSubmitting, setFormIsSubmitting] = useState(false)
+  const onSubmit = res => {
+    if (res.isFormValid) {
+      setFormIsSubmitting(true)
+    }
+  }
+  const postSubmit = res => {
+    setFormIsSubmitting(false)
+  }
+
   return (
     <div id='app'>
       <h1>Tweet Chart</h1>
+
+      <Form
+        onSubmit={onSubmit}
+        postSubmit={postSubmit}
+        postOptions={{ method: 'post', url: '/get-image' }}
+      >
+        <fieldset disabled={formIsSubmitting}>
+          <div className='formGroup'>
+            <label htmlFor='tweetUrl'>
+              Tweet URL
+            </label>
+            <Input
+              type='url'
+              id='tweetUrl'
+              name='tweetUrl'
+              validations={validations.empty}
+              placeholder='https://twitter.com/elonmusk/status/1366477626429169666'
+            />
+          </div>
+
+          <div className='formGroup'>
+            <button>Submit</button>
+          </div>
+        </fieldset>
+      </Form>
     </div>
   )
 }

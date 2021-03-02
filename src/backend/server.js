3e321e1b9d18d7efaa43277c5c600b1e9c5b2929
Ozgur <ozgr@live.com>
2021-03-03 01:12:56 +0300

    Add dotenv

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 1300
 
+require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') })
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
 

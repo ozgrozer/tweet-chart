86a37caffe25056660534ff2fe0eff1b6177a6e2
Ozgur <ozgr@live.com>
2021-03-03 02:33:14 +0300

    Add simple line chart

--- src/frontend/js/index.jsx
+++ src/frontend/js/index.jsx
@@ -1,8 +1,45 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Form, Input, Select } from 'rfv'
+import { Line } from '@reactchartjs/react-chart.js'
 
 import './../css/style.scss'
+
+const LineChart = () => {
+  const btc = {
+    labels: ['Jan 1 2021', 'Jan 2 2021', 'Jan 3 2021', 'Jan 4 2021', 'Jan 5 2021', 'Jan 6 2021', 'Jan 7 2021', 'Jan 8 2021', 'Jan 9 2021', 'Jan 10 2021', 'Jan 11 2021', 'Jan 12 2021', 'Jan 13 2021', 'Jan 14 2021', 'Jan 15 2021', 'Jan 16 2021', 'Jan 17 2021', 'Jan 18 2021', 'Jan 19 2021', 'Jan 20 2021', 'Jan 21 2021', 'Jan 22 2021', 'Jan 23 2021', 'Jan 24 2021', 'Jan 25 2021', 'Jan 26 2021', 'Jan 27 2021', 'Jan 28 2021', 'Jan 29 2021', 'Jan 30 2021', 'Jan 31 2021'],
+    data: ['29380.86', '32174.21', '33047.33', '32060.62', '34030.85', '36761.82', '39638.67', '40868.37', '40358.75', '38338.40', '35687.10', '34512.57', '37165.51', '39122.75', '36877.52', '36061.34', '35836.69', '36593.99', '35982.88', '35538.56', '31060.53', '32890.20', '32115.08', '32279.40', '32337.20', '32498.71', '30491.49', '33404.37', '34218.52', '34180.87', '33063.63']
+  }
+
+  const data = {
+    labels: btc.labels,
+    datasets: [
+      {
+        fill: false,
+        label: 'BTC',
+        data: btc.data,
+        pointRadius: 0,
+        borderColor: '#4b64ce'
+      }
+    ]
+  }
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false
+          }
+        }
+      ]
+    }
+  }
+
+  return (
+    <Line data={data} options={options} />
+  )
+}
 
 const validations = {
   empty: [
@@ -101,6 +138,10 @@ const App = () => {
               </div>
               )
         }
+      </div>
+
+      <div style={{ marginTop: '30px' }}>
+        <LineChart />
       </div>
     </div>
   )

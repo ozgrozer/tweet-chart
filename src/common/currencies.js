86ba96d01b07b63e7d419eaf76538719ca495a9e
Ozgur <ozgr@live.com>
2021-03-20 23:43:45 +0300

    Add some currencies

---
+++ src/common/currencies.js
@@ -0,0 +1,14 @@
+const currencies = [
+  { value: 'BTC', label: 'Bitcoin BTC' },
+  { value: 'ETH', label: 'Ethereum ETH' },
+  { value: 'BNB', label: 'Binance Coin BNB' },
+  { value: 'ADA', label: 'Cardano ADA' },
+  { value: 'USDT', label: 'Tether USDT' },
+  { value: 'Dot', label: 'Polkadot Dot' },
+  { value: 'XRP', label: 'XRP XRP' },
+  { value: 'UNI', label: 'Uniswap UNI' },
+  { value: 'LTC', label: 'Litecoin LTC' },
+  { value: 'LINK', label: 'Chainlink LINK' }
+]
+
+module.exports = currencies
